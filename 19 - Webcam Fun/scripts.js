aea20af199f30d73f002ca64d8ae3686da60353b
Henry Chen <henrychen1@gmail.com>
2018-03-17 21:07:50 -0700

    updated

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -28,3 +28,21 @@ function paintToCanavas() {
     }, 16);
 }
 
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+
+    //take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+    //link.textContent = 'Download Image';
+    strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanavas);
+
